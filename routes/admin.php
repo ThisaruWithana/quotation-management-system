9a8339f8197d58359a0c6bf2709599ea000d8ce4
dilini.fernando <dilini.fernando@debugsire.com>
2024-06-16 20:59:54 +0100

    quotation print space issue

--- routes/admin.php
+++ routes/admin.php
@@ -25,14 +25,12 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::middleware(['role:admin'])->group(function(){
-        // Route::resource('user',UserController::class);
-        // Route::resource('role',RoleController::class);
+    Route::middleware(['check-permission'])->group(function(){
         Route::resource('permission',PermissionController::class);
-        Route::resource('subcategory',SubCateoryController::class);
-        Route::resource('collection',CollectionController::class);
-        Route::resource('product',ProductController::class);
-        Route::get('/get/subcategory',[ProductController::class,'getsubcategory'])->name('getsubcategory');
+        // Route::resource('subcategory',SubCateoryController::class);
+        // Route::resource('collection',CollectionController::class);
+        // Route::resource('product',ProductController::class);
+        // Route::get('/get/subcategory',[ProductController::class,'getsubcategory'])->name('getsubcategory');
         Route::get('/remove-external-img/{id}',[ProductController::class,'removeImage'])->name('remove.image');
     });
 
@@ -41,34 +39,31 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(
         Route::resource('role',RoleController::class);
         Route::resource('location',LocationController::class);
         Route::post('location/change-status', [LocationController::class, 'changeStatus'])->name('location.change-status');
+        Route::resource('vat',VatController::class);
+        
+        Route::resource('supplier',SupplierController::class);
+        Route::post('supplier/change-status', [SupplierController::class, 'changeStatus'])->name('supplier.change-status');
+        
+        Route::resource('department',DepartmentController::class);
+        Route::post('department/change-status', [DepartmentController::class, 'changeStatus'])->name('department.change-status');
+
+        Route::get('department/sub/index', [DepartmentController::class, 'listSubDepartment'])->name('department.sub.index');
+        Route::get('department/sub/create', [DepartmentController::class, 'createSubDepartment'])->name('department.sub.create');
+        Route::post('department/sub/store', [DepartmentController::class, 'storeSubDepartment'])->name('department.sub.store');
+        Route::get('department/sub/edit/{id}', [DepartmentController::class, 'editSubDepartment'])->name('department.sub.edit');
+        Route::post('department/sub/change-status', [DepartmentController::class, 'changeStatusSubDepartments'])->name('department.sub.change-status');
+        Route::post('department/get-subdepartments-by-departments', [DepartmentController::class, 'getSubDepartmentByDept'])->name('department.get-subdepartments-by-departments');
+        Route::post('department/get-vat-value', [DepartmentController::class, 'getVatValue'])->name('department.get-vat-value');
+
+        Route::resource('customer',CustomerController::class);
+        Route::post('customer/change-status', [CustomerController::class, 'changeStatus'])->name('customer.change-status');
+        Route::post('customer/get-details', [CustomerController::class, 'getDetails'])->name('customer.get-details');
+
+        Route::post('user/change-status', [UserController::class, 'changeStatus'])->name('user.change-status');
+        Route::post('role/change-status', [RoleController::class, 'changeStatus'])->name('role.change-status');
+        Route::post('permission/change-status', [PermissionController::class, 'changeStatus'])->name('permission.change-status');
+
     });
-
-    Route::resource('vat',VatController::class);
-
-    Route::resource('supplier',SupplierController::class);
-    Route::post('supplier/change-status', [SupplierController::class, 'changeStatus'])->name('supplier.change-status');
-
-    
-    Route::resource('department',DepartmentController::class);
-    Route::post('department/change-status', [DepartmentController::class, 'changeStatus'])->name('department.change-status');
-
-    Route::get('department/sub/index', [DepartmentController::class, 'listSubDepartment'])->name('department.sub.index');
-    Route::get('department/sub/create', [DepartmentController::class, 'createSubDepartment'])->name('department.sub.create');
-    Route::post('department/sub/store', [DepartmentController::class, 'storeSubDepartment'])->name('department.sub.store');
-    Route::get('department/sub/edit/{id}', [DepartmentController::class, 'editSubDepartment'])->name('department.sub.edit');
-    Route::post('department/sub/change-status', [DepartmentController::class, 'changeStatusSubDepartments'])->name('department.sub.change-status');
-    Route::post('department/get-subdepartments-by-departments', [DepartmentController::class, 'getSubDepartmentByDept'])->name('department.get-subdepartments-by-departments');
-    Route::post('department/get-vat-value', [DepartmentController::class, 'getVatValue'])->name('department.get-vat-value');
-
-
-    Route::resource('customer',CustomerController::class);
-    Route::post('customer/change-status', [CustomerController::class, 'changeStatus'])->name('customer.change-status');
-    Route::post('customer/get-details', [CustomerController::class, 'getDetails'])->name('customer.get-details');
-
-    Route::post('user/change-status', [UserController::class, 'changeStatus'])->name('user.change-status');
-    Route::post('role/change-status', [RoleController::class, 'changeStatus'])->name('role.change-status');
-    Route::post('permission/change-status', [PermissionController::class, 'changeStatus'])->name('permission.change-status');
-    
     
     Route::resource('bundle',BundleController::class);
     Route::post('bundle/add-items', [BundleController::class, 'addItems'])->name('bundle.add-items');
@@ -205,5 +200,3 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(
     });
 
 });
-
-Route::get('/barcode', [VatController::class, 'barcode']);
