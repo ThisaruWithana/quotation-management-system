0afd135e486e463e24e096b72ad2561776176fba
dilini.fernando <dilini.fernando@debugsire.com>
2024-10-13 12:51:00 +0100

    opf bug fix , session timeout bug fixed

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,11 +21,11 @@ class Authenticate
     public function handle($request, Closure $next)
     {
         $isLoggedIn = $request->path() != 'dashboard/logout';
-
-        if(! session('lastActivityTime'))
+// var_dump(time() - $this->session->get('lastActivityTime')); die();
+        if(!session('lastActivityTime')){
             $this->session->put('lastActivityTime', time());
 
-        elseif(time() - $this->session->get('lastActivityTime') > $this->timeout){
+        }elseif(time() - $this->session->get('lastActivityTime') > $this->timeout){
 
             $this->session->forget('lastActivityTime');
             $cookie = cookie('intend', $isLoggedIn ? url()->current() : 'dashboard');
